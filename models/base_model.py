8336d5fae5283402c8913ea4b69e2866400ff806
Rahmat <rahmatbola3@gmail.com>
2024-02-08 19:38:55 -0800

    AirBnB

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+import json
+"""
+A class called BaseModel
+"""
+
+
+class BaseModel:
+    """
+    A BaseModel class that defines common attributes/methods for other classes.
+    """
+    def __init__(self):
+        """
+        Initializes a BaseModel instance with id,
+        when it was created(created_at),
+        and when it was updated(updated_at) attributes.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        converts the BaseModel instance to string representation.
+        """
+        return ("[{}] ({}) {}"
+                .format(type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+        Updates the attribute 'updated_at' with the current datetime.
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of the instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
