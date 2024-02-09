dba7b96d28be513dedddadac091eb923b354bd1b
Rahmat <rahmatbola3@gmail.com>
2024-02-09 04:04:30 -0800

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,33 @@ class BaseModel:
     """
     A BaseModel class that defines common attributes/methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Initializes a BaseModel instance with id,
-        when it was created(created_at),
-        and when it was updated(updated_at) attributes.
+        Initializes a BaseModel instance.
+
+        Args:
+            *args: Variable length argument list (not used).
+            **kwargs: Arbitrary keyword arguments.
+
+        If kwargs is not empty:
+            - Each key of this dictionary is an attribute name (Note __class__ from kwargs is not added as an attribute).
+            - Each value of this dictionary is the value of the corresponding attribute.
+            - Convert created_at and updated_at strings into datetime objects.
+        Otherwise:
+            - Create id and created_at as previously (new instance).
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue  # Skip __class__ attribute
+                elif key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
