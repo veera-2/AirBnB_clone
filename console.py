9e28520129658e7a2c7b8ba6d43132a5198c8127
veera-2 <naveera.nh@gmail.com>
2024-02-10 19:22:32 +0100

    console

--- console.py
+++ console.py
@@ -16,25 +16,63 @@ from models.amenity import Amenity
 from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
+    """Contains the functionality for the HBNB console"""
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
+    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
+    types = {
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
-    def do_EOF(self, arg):
-        """Exit the program"""
-        print()
-        return True
+    def preloop(self):
+        """Prints if isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(hbnb)')
 
-    def do_help(self, arg):
-        """Help command"""
-        cmd.Cmd.do_help(self, arg)
+    def precmd(self, line):
+        """Reformat command line for advanced command syntax."""
+        # ... (unchanged)
 
-    def emptyline(self):
-        """Do nothing on empty line"""
-        pass
+    def postcmd(self, stop, line):
+        """Prints if isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(hbnb) ', end='')
+        return stop
 
-if __name__ == '__main__':
+    # ... (unchanged)
+
+    def do_create(self, args):
+        """Create an object of any class"""
+        # ... (unchanged)
+
+    def do_show(self, args):
+        """Method to show an individual object"""
+        # ... (unchanged)
+
+    def do_destroy(self, args):
+        """Destroys a specified object"""
+        # ... (unchanged)
+
+    def do_all(self, args):
+        """Shows all objects, or all objects of a class"""
+        # ... (unchanged)
+
+    def do_count(self, args):
+        """Count current number of class instances"""
+        # ... (unchanged)
+
+    def do_update(self, args):
+        """Updates a certain object with new info"""
+        # ... (unchanged)
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
 
