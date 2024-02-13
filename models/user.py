8cf9e95ab33455dd7274ae7907f7e7cefad7cfd4
Rahmat <rahmatbola3@gmail.com>
2024-02-12 20:22:05 -0800

    pyStyle

--- models/user.py
+++ models/user.py
@@ -1,41 +1,20 @@
-#!/usr/bin/python3
-""" holds class User"""
-import hashlib
-from os import getenv
+#!/usr/bin/pyton3
+""" User Model module.
+Describes the User class that inherits from BaseModel.
+"""
+from models.base_model import BaseModel
 
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128),
-                       nullable=False)
-        _password = Column('password',
-                           String(128),
-                           nullable=False)
-        first_name = Column(String(128),
-                            nullable=True)
-        last_name = Column(String(128),
-                           nullable=True)
-        places = relationship("Place",
-                              backref="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review",
-                               backref="user",
-                               cascade="all, delete-orphan")
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
+class User(BaseModel):
+    """This class defines the User.
+    Attributes:
+        email (str): the user's email.
+        password (str): the user's password.
+        first_name (str): the user's first_name.
+        last_name (str): the user's last_name.
+    """
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
